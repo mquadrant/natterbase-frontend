76067de3e54897d2c6e406316460ae7ada7c7648
Benjamin Mark <mquadrant@gmail.com>
2019-10-10 19:43:22 +0100

    created horizontal line component

---
+++ src/components/HorizontalLine.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HorizontalLine() {
+  return (
+    <hr
+      style={{
+        borderColor: "rgba(197, 197, 197, 0.29)"
+      }}
+    />
+  );
+}
