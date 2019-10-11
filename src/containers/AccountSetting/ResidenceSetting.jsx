f34aabc7ecb33be7b9364cf20678075c1b075c96
Benjamin Mark <mquadrant@gmail.com>
2019-10-11 17:19:14 +0100

    set state for residence setting form fields

--- src/containers/AccountSetting/ResidenceSetting.jsx
+++ src/containers/AccountSetting/ResidenceSetting.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Bullet from "./../../images/bullet.svg";
 import { Card, Col, Form, Button } from "react-bootstrap";
 
 export default function ResidenceSetting() {
+  const [values, setValues] = useState({
+    currentState: "Niger",
+    newState: "",
+    lgaLive: "",
+    fedConsti: "",
+    senDistrict: "",
+    stateConsti: ""
+  });
+
+  const handleChange = e => {
+    const { value, name } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <Card className="setting-card">
       <Card.Body>
@@ -22,7 +39,12 @@ export default function ResidenceSetting() {
               controlId="formGridEmail"
             >
               <Form.Label>CURRENT STATE</Form.Label>
-              <Form.Control disabled type="text" value="Niger" />
+              <Form.Control
+                disabled
+                type="text"
+                value={values.currentState}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Form.Row>
           <Form.Row className="my-2">
@@ -33,7 +55,12 @@ export default function ResidenceSetting() {
               controlId="formGridFname"
             >
               <Form.Label>NEW STATE OF RESIDENCE</Form.Label>
-              <Form.Control type="text" value="" />
+              <Form.Control
+                type="text"
+                name="newState"
+                value={values.newState}
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group
               as={Col}
@@ -42,7 +69,12 @@ export default function ResidenceSetting() {
               controlId="formGridUname"
             >
               <Form.Label>WHAT LGA DO YOU LIVE IN</Form.Label>
-              <Form.Control type="text" value="" />
+              <Form.Control
+                type="text"
+                name="lgaLive"
+                value={values.lgaLive}
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group
               as={Col}
@@ -51,7 +83,12 @@ export default function ResidenceSetting() {
               controlId="formGridDbirth"
             >
               <Form.Label>FEDERAL CONSTITUENCIES</Form.Label>
-              <Form.Control type="text" value="" />
+              <Form.Control
+                type="text"
+                name="fedConsti"
+                value={values.fedConsti}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Form.Row>
           <Form.Row className="my-2">
@@ -62,7 +99,12 @@ export default function ResidenceSetting() {
               controlId="formGridFname"
             >
               <Form.Label>SENETORIAL DISTRICT</Form.Label>
-              <Form.Control type="text" value="" />
+              <Form.Control
+                type="text"
+                name="senDistrict"
+                value={values.senDistrict}
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group
               as={Col}
@@ -71,10 +113,15 @@ export default function ResidenceSetting() {
               controlId="formGridUname"
             >
               <Form.Label>STATE CONSTITUENCIES</Form.Label>
-              <Form.Control type="text" value="" />
+              <Form.Control
+                type="text"
+                name="stateConsti"
+                value={values.stateConsti}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Form.Row>
-          <Button className="my-2" type="submit">
+          <Button className="my-2" type="submit" onClick={handleSubmit}>
             Save Changes
           </Button>
         </Form>
