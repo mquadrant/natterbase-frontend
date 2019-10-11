10d34fd5e004d51d6fab489706c61f7dd52f4fa1
Benjamin Mark <mquadrant@gmail.com>
2019-10-11 09:55:06 +0100

    configured axios

---
+++ src/utils/API.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYjg2NWZmMzFhZWRkMzQxNDU0OTQ2YiIsImlhdCI6MTU2NTk0Nzg1NCwiZXhwIjoxNTY1OTU1MDU0fQ.5ujc_dMloqYdyoQN556PhGwIqa1OfGwt06vD5iKu-lg";
+export default axios.create({
+  baseURL: "http://test.natterbase.com:5050",
+  responseType: "json",
+  headers: { token: `${token}` }
+});
