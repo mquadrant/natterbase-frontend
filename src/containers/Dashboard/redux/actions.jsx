ddfebb48e29fe95f31c3bc59e3a058cde87fb7fb
Benjamin Mark <mquadrant@gmail.com>
2019-10-11 11:35:23 +0100

    refactored code

--- src/containers/Dashboard/redux/actions.jsx
+++ src/containers/Dashboard/redux/actions.jsx
@@ -43,7 +43,7 @@ export function getStatisticsError(error) {
   return { type: types.GET_STATISTICS_PENDING, error };
 }
 
-export const getApplications = () => {
+export const getStatistics = () => {
   return dispatch => {
     dispatch(getStatisticsPending());
     return axios
