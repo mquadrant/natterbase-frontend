f91dc5872e234fcf43616b533e4e97dc315ea7ce
Benjamin Mark <mquadrant@gmail.com>
2019-10-11 11:34:43 +0100

    added actions to get statistics and add to store

--- src/containers/Dashboard/redux/actions.jsx
+++ src/containers/Dashboard/redux/actions.jsx
@@ -1,6 +1,7 @@
 import axios from "./../../../utils/API";
 import * as types from "./types";
 
+/* APPLICATIONS ACTIONS */
 export function getApplicationAction(payload) {
   return { type: types.GET_ALL_APPLICATION, payload };
 }
@@ -19,11 +20,40 @@ export const getApplications = () => {
     return axios
       .get("/api/interview/get-applications")
       .then(res => {
-        dispatch(getApplicationAction(res.data.data));
-        return res.data.data;
+        dispatch(getApplicationAction(res.data.applications));
+        return res.data;
       })
       .catch(error => {
         dispatch(getApplicationError(error));
+        throw error;
+      });
+  };
+};
+
+/* STATISTICS ACTIONS */
+export function getStatisticsAction(payload) {
+  return { type: types.GET_STATISTICS, payload };
+}
+
+export function getStatisticsPending() {
+  return { type: types.GET_STATISTICS_PENDING };
+}
+
+export function getStatisticsError(error) {
+  return { type: types.GET_STATISTICS_PENDING, error };
+}
+
+export const getApplications = () => {
+  return dispatch => {
+    dispatch(getStatisticsPending());
+    return axios
+      .get("/api/interview/get-statistics")
+      .then(res => {
+        dispatch(getStatisticsAction(res.data.statistics));
+        return res.data;
+      })
+      .catch(error => {
+        dispatch(getStatisticsError(error));
         throw error;
       });
   };
