f8cc7c6e6f15f3011c8aa95e125d1c217ee40112
Benjamin Mark <mquadrant@gmail.com>
2019-10-11 16:36:52 +0100

    populated the statistic section

--- src/containers/Dashboard/DashboardPage.jsx
+++ src/containers/Dashboard/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageTitle from "./../../components/PageTitle";
 import Spacer from "./../../components/Spacer";
 import { connect } from "react-redux";
@@ -8,6 +8,10 @@ import StatisticCard from "./StatisticCard";
 import ApplicationTable from "./ApplicationTable";
 import HorizontalLine from "../../components/HorizontalLine";
 import { getApplications, getStatistics } from "./redux/actions";
+import {
+  currencyConverter,
+  numberConverter
+} from "./../../helpers/currencyConverter";
 import BGCard1 from "./../../images/cardbackground1.svg";
 import BGCard2 from "./../../images/cardbackground2.svg";
 import BGCard3 from "./../../images/cardbackground3.svg";
@@ -16,12 +20,47 @@ import Card1 from "./../../images/card1.svg";
 import Card2 from "./../../images/card2.svg";
 import Card3 from "./../../images/card3.svg";
 import Card4 from "./../../images/card4.svg";
+import { ErrorAlert, Loader } from "../../components/UtilComponent";
 
 function DashboardPage(props) {
   const { getStatisticsHandler, getApplicationsHandler } = props;
+  const { statistics, statisticsError, statisticsPending } = props;
 
-  getStatisticsHandler();
-  getApplicationsHandler();
+  const [statData, setStatData] = useState([]);
+
+  useEffect(() => {
+    getStatisticsHandler();
+    getApplicationsHandler();
+  }, [getStatisticsHandler, getApplicationsHandler]);
+
+  useEffect(() => {
+    setStatData([
+      {
+        bgImg: BGCard1,
+        buImg: Card1,
+        title: numberConverter(statistics.totalInsurancesBought),
+        desc: "Total Insurance Bought"
+      },
+      {
+        bgImg: BGCard2,
+        buImg: Card2,
+        title: `₦${currencyConverter(statistics.totalInsuranceAmount)}`,
+        desc: "Total Amount of Insurance"
+      },
+      {
+        bgImg: BGCard3,
+        buImg: Card3,
+        title: numberConverter(statistics.totalInsuranceClaims),
+        desc: "Total Insurance Claims"
+      },
+      {
+        bgImg: BGCard4,
+        buImg: Card4,
+        title: `₦${currencyConverter(statistics.totalClaimsAmount)}`,
+        desc: "Total Amount of Claims"
+      }
+    ]);
+  }, [statistics]);
 
   return (
     <div className="dashboard">
@@ -31,30 +70,23 @@ function DashboardPage(props) {
         <p className="date">{format(new Date(), "EEEE, MMMM do yyyy")}.</p>
         <Spacer size="30" />
         <section className="statistics d-flex flex-wrap">
-          <StatisticCard
-            bgImage={BGCard1}
-            bulletImage={Card1}
-            title={"06"}
-            desc={"Total Insurance Bought"}
-          />
-          <StatisticCard
-            bgImage={BGCard2}
-            bulletImage={Card2}
-            title={"06"}
-            desc={"Total Insurance Bought"}
-          />
-          <StatisticCard
-            bgImage={BGCard3}
-            bulletImage={Card3}
-            title={"06"}
-            desc={"Total Insurance Bought"}
-          />
-          <StatisticCard
-            bgImage={BGCard4}
-            bulletImage={Card4}
-            title={"06"}
-            desc={"Total Insurance Bought"}
-          />
+          {statisticsPending ? (
+            <div style={{ textAlign: "center", width: "100%" }}>
+              <Loader />
+            </div>
+          ) : statisticsError ? (
+            <ErrorAlert />
+          ) : (
+            statData.map((stat, index) => (
+              <StatisticCard
+                key={index}
+                bgImage={stat.bgImg}
+                bulletImage={stat.buImg}
+                title={stat.title}
+                desc={stat.desc}
+              />
+            ))
+          )}
         </section>
         <Spacer size="40" />
         <section className="application-table card">
@@ -70,12 +102,17 @@ function DashboardPage(props) {
   );
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//       error: state.journals.error,
-//       pending: state.journals.pending,
-//   };
-// };
+const mapStateToProps = state => {
+  return {
+    applications: state.applications.applications,
+    applicationError: state.applications.applicationError,
+    applicationPending: state.applications.applicationPending,
+
+    statistics: state.statistics.statistics,
+    statisticsError: state.statistics.statisticsError,
+    statisticsPending: state.statistics.statisticsPending
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -85,6 +122,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(DashboardPage);
